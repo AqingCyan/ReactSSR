90a9e46f8fdddfbafc70169984f122ba9d3983ac
AqingCyan <aqingcyan@cyanthing.com>
2020-01-28 15:37:11 +0800

    🥕 配置工程实现简单的同构

--- src/index.js
+++ src/index.js
@@ -3,9 +3,25 @@ import React from 'react'
 import ReactDomServer from 'react-dom/server'
 import Home from './containers/Home'
 
-const { renderToString } = ReactDomServer
 const app = Express()
+app.use(Express.static('public'))
 
-app.get('/', (req, res) => res.send(renderToString(<Home />)))
+const { renderToString } = ReactDomServer
+const content = renderToString(<Home />)
+
+app.get('/', (req, res) => res.send(
+  `
+    <html>
+      <head>
+        <link rel="icon" href="https://zh-hans.reactjs.org/favicon.ico"/>
+        <title>React SSR</title>
+      </head>
+        <body>
+           <div id="root">${content}</div>
+           <script src="index.js"></script>
+        </body>
+    </html>
+  `,
+))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- webpack.client.js
+++ webpack.client.js
@@ -1,15 +1,12 @@
 const path = require('path')
-const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
-  target: 'node',
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/client/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'public'),
   },
-  externals: [nodeExternals()],
   module: {
     rules: [{
       test: /\.js?$/,
