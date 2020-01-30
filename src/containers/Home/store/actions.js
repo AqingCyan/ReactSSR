e6eeb28cc393d35f846b4230ee25410f4958be97
AqingCyan <aqingcyan@cyanthing.com>
2020-01-30 21:56:32 +0800

    🎭 使用redux-thunk中的withExtraArgument来优化axiosInstance的使用

--- src/containers/Home/store/actions.js
+++ src/containers/Home/store/actions.js
@@ -1,17 +1,14 @@
 import { CHANGE_LIST } from './constants'
-import clientAxios from '../../../client/request'
-import serverAxios from '../../../server/request'
 
 const changeList = (list) => ({
   type: CHANGE_LIST,
   list,
 })
 
-export const getHomeList = (server) => (dispatch) => {
-  const request = server ? serverAxios : clientAxios
-  return request.get('/api/news.json?secret=PP87ANTIPIRATE')
+export const getHomeList = () => (dispatch, getState, axiosInstance) => (
+  axiosInstance.get('/api/news.json?secret=PP87ANTIPIRATE')
     .then((res) => {
       const list = res.data.data
       dispatch(changeList(list))
     })
-}
+)
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,21 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { reducer as homeReducer } from '../containers/Home/store'
+import serverAxios from '../server/request'
+import clientAxios from '../client/request'
 
 const reducer = combineReducers({
   home: homeReducer,
 })
 
 // 避免所有render公用一个store
-export const getStore = () => createStore(reducer, applyMiddleware(thunk))
+export const getStore = () => (
+  // 改变服务器端的store的内容，就一定要使用serverAxios
+  createStore(reducer, applyMiddleware(thunk.withExtraArgument(serverAxios)))
+)
+
 export const getClientStore = () => {
+  // 改变客户端的store的内容，就一定要使用clientAxios
   const defaultState = window.context.state
-  return createStore(reducer, defaultState, applyMiddleware(thunk))
+  return createStore(reducer, defaultState, applyMiddleware(thunk.withExtraArgument(clientAxios)))
 }
